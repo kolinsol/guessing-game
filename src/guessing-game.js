07a73017a737f79b1e43b2f0ef8f958d0246c631
Yauheni Tsiarokhin <hihihiko00@gmail.com>
2017-09-16 04:57:01 +0300

    solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,38 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.min = null;
+      this.max = null;
+      this.currentGuess = null;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.currentGuess = Math.ceil((this.max - this.min) / 2) + this.min;
+      return this.currentGuess;
     }
 
     lower() {
-
+      this.max = this.currentGuess;
     }
 
     greater() {
-
+      this.min = this.currentGuess;
     }
+}
+
+let a = new GuessingGame();
+let n = 12345;
+let result = null;
+a.setRange(0, 200000);
+while(result !== n) {
+  result = a.guess();
+  console.log(result);
+  if (result > n) a.lower();
+  if (result < n) a.greater();
 }
 
 module.exports = GuessingGame;
